67d0ba7d3128fab28701cb594e6068bf2acf53c7
Boris Glass <boris@glass.li>
2020-08-21 10:19:06 +0200

    fix: url->path

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
+
 from tutorials import views
 
 urlpatterns = [
-    url(r'^api/tutorials$', views.tutorial_list),
-    url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
-    url(r'^api/tutorials/published$', views.tutorial_list_published)
+    path('api/tutorials', views.tutorial_list),
+    path('api/tutorials/(?P<pk>[0-9]+)', views.tutorial_detail),
+    path('api/tutorials/published', views.tutorial_list_published)
 ]
