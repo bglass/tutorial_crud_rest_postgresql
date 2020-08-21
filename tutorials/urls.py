a846d92f5e9ae8679c3cc584ef1a6a6b76ac3209
Boris Glass <boris@glass.li>
2020-08-21 13:19:17 +0200

    structure: + /frontend

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -4,6 +4,6 @@ from tutorials import views
 
 urlpatterns = [
     path('api/tutorials', views.tutorial_list),
-    path('api/tutorials/(?P<pk>[0-9]+)', views.tutorial_detail),
+    path('api/tutorials/<int:pk>', views.tutorial_detail),
     path('api/tutorials/published', views.tutorial_list_published)
 ]
